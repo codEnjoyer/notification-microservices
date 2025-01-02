c95795e03677350455a7850f9f605d2c5afcb74b
MagNull <pathfo@yandex.ru>
2025-01-02 15:39:16 +0500

    Remove test

--- SendServices/Program.cs
+++ SendServices/Program.cs
@@ -5,11 +5,11 @@ var kafkaProducer = new KafkaProducer();
 var gateway = new NotificationGateway(kafkaProducer);
 var notificationConsumer = new NotificationConsumer(gateway);
 Console.WriteLine("Start Listen messages");
-
-using StreamReader sr =
-    new StreamReader("C:\\Users\\pathf\\RiderProjects\\SendServices\\SendServices\\testRequest.json");
-var json = sr.ReadToEnd();
-kafkaProducer.TestProduce("send_request", json);
+//
+// using StreamReader sr =
+//     new StreamReader("C:\\Users\\pathf\\RiderProjects\\SendServices\\SendServices\\testRequest.json");
+// var json = sr.ReadToEnd();
+// kafkaProducer.TestProduce("send_request", json);
 
 await notificationConsumer.ConsumeAsync();
 
