e347ed728a86c61ae61b5c25850c1f30b3c30a38
MagNull <pathfo@yandex.ru>
2025-01-02 15:57:04 +0500

    send_response key fix

--- SendServices/Program.cs
+++ SendServices/Program.cs
@@ -5,7 +5,7 @@ var kafkaProducer = new KafkaProducer();
 var gateway = new NotificationGateway(kafkaProducer);
 var notificationConsumer = new NotificationConsumer(gateway);
 Console.WriteLine("Start Listen messages");
-//
+
 // using StreamReader sr =
 //     new StreamReader("C:\\Users\\pathf\\RiderProjects\\SendServices\\SendServices\\testRequest.json");
 // var json = sr.ReadToEnd();
