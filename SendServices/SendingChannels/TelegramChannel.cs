b7cf451fe9ffe98c73878d68963570ae13c54ca2
MagNull <pathfo@yandex.ru>
2025-01-02 15:46:13 +0500

    Add send by UserId telegram

--- SendServices/SendingChannels/TelegramChannel.cs
+++ SendServices/SendingChannels/TelegramChannel.cs
@@ -7,7 +7,7 @@ public class TelegramChannel : ISendingChannel
 {
     private const string ApiToken = "7770806075:AAHvbaYNTvaixuSTVrXYoSv3MFQ_GyFKKn4";
     private readonly TelegramBotClient _telegramBotClient = new(ApiToken);
-    private readonly HashSet<Chat> _chats = new();
+    private readonly HashSet<(Chat chat, Contact contact)> _chats = new();
 
     public bool CanSend(string type) => type.ToLower() == "telegram";
 
@@ -15,21 +15,22 @@ public class TelegramChannel : ISendingChannel
     {
         _telegramBotClient.OnMessage += (message, type) =>
         {
-            _chats.Add(message.Chat);
-            
+            _chats.Add((message.Chat, message.Contact)!);
+
             return Task.CompletedTask;
         };
     }
 
-    public string Send(string userName, string message)
+    public string Send(string address, string message)
     {
-        var destinationChat = _chats.FirstOrDefault(id => id.Username == userName);
-        if (destinationChat == default)
+        var destination =
+            _chats.FirstOrDefault(id => id.chat.Username == address || id.contact.UserId.ToString() == address);
+        if (destination == default)
         {
             return "Unknown user address.";
         }
 
-        _telegramBotClient.SendMessage(destinationChat, message);
+        _telegramBotClient.SendMessage(destination.chat, message);
         return "OK";
     }
 }
